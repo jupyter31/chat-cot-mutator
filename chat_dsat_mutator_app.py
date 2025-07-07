68c5d459e10ef5f97962140f127ea164a87e895a
Jess Peck <t-jesspeck@microsoft.com>
2025-07-07 11:35:35 +0100

    Define args needed for main controller function

--- chat_dsat_mutator_app.py
+++ chat_dsat_mutator_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import json
-from chat_dsat_mutator_controller import foo
+from chat_dsat_mutator_controller import mutate_chat_sample
 
 
 
@@ -8,17 +8,19 @@ st.header("Synthetic Chat-Data Mutation Framework")
 
 # get chat sample input from file upload or text area
 st.subheader("Chat sample")
-uploaded_file = st.file_uploader("Upload a JSON file", type=["json"])
-chat_sample = ""
+# uploaded_file = st.file_uploader("Upload a JSON file", type=["json"])
+# chat_sample = ""
 
-if uploaded_file is not None:
-    try:
-        chat_sample = json.load(uploaded_file)
-        chat_sample = json.dumps(chat_sample, indent=2)
-    except Exception as e:
-        st.error(f"Error reading JSON file: {e}")
-else:
-    chat_sample = st.text_area("Paste chat sample here")
+# if uploaded_file is not None:
+#     try:
+#         chat_sample = json.load(uploaded_file)
+#         chat_sample = json.dumps(chat_sample, indent=2)
+#     except Exception as e:
+#         st.error(f"Error reading JSON file: {e}")
+# else:
+#     chat_sample = st.text_area("Paste chat sample here")
+
+chat_sample = st.text_area("Paste chat sample here")
 
 st.session_state["chat_sample"] = chat_sample
 
@@ -27,5 +29,7 @@ st.subheader("Mutation request")
 #mutation_request = st.text_input("Enter mutation request", placeholder="e.g. 'Inject a hallucination'")
 options = ["Misattribution", "Hallucination", "Policy edge-cases", "Persona shift"]
 mutation_request = st.selectbox("Select mutation type", options, accept_new_options=False)
+
+mutation = mutate_chat_sample(chat_sample, mutation_request)
 
 
--- chat_dsat_mutator_controller.py
+++ chat_dsat_mutator_controller.py
@@ -1,2 +1,15 @@
-def foo():
-    return
+def mutate_chat_sample(chat_sample, mutation_request):
+    """
+    Mutates the chat sample based on the mutation request.
+    
+    Args:
+        chat_sample (str): The chat sample in ??? format.
+        mutation_request (str): The type of mutation to apply.
+        
+    Returns:
+        str: The mutated chat sample in ??? format.
+    """
+
+    
+
+    return ""
